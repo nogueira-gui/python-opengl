ce259c6cbd0dd0cabc33fd3ccfcabb2d52f4d504
Guilherme Nogueira <guilherme.nogueiras@outlook.com>
2023-09-09 16:02:59 -0300

    feat: rotação triangulo

--- triangle.py
+++ triangle.py
@@ -1,3 +1,4 @@
+import math
 import pygame as pg
 from OpenGL.GL import *
 
@@ -6,18 +7,18 @@ class Triangle:
     # Equilateral triangle points
     points = [
         {
-            'x': 0,
-            'y': 0.5,
+            'x': -0.25,
+            'y': 0.1445,
             'color': [1.0, 0.0, 0.0]
         },
         {
-            'x': -0.433,
-            'y': -0.25,
+            'x': 0.25,
+            'y': 0.1445,
             'color': [0.0, 1.0, 0.0]
         },
         {
-            'x': 0.433,
-            'y': -0.25,
+            'x': 0.0,
+            'y': -0.2885,
             'color': [0.0, 0.0, 1.0]
         }
     ]
@@ -40,15 +41,35 @@ class Triangle:
                 if event.type == pg.QUIT:
                     running = False
             glClear(GL_COLOR_BUFFER_BIT)
-            self.draw_triangle()
+            self.draw_triangle(0, 0, 0) #triangulo equilatero normal
+            self.draw_triangle(0.50, 0.30, 0) #triangulo transladado
+            self.draw_triangle(0.50, -0.40, 90.0) #triangulo rotacionado
             pg.display.flip()
             self.clock.tick(60)
         self.quit()
 
-    def draw_triangle(self):
+    def draw_triangle(self, dx, dy, r):
+        center_x = 0.0 + dx #coordenada x do centro de rotação
+        center_y = 0.0 + dy #coordenada y do centro de rotação
+
         glBegin(GL_TRIANGLES)
         for point in self.points:
-            glVertex2f(point['x'], point['y'])
+            point_x = point['x'] + dx
+            point_y = point['y'] + dy
+
+            # Calcula as coordenadas em relação ao centro de rotação
+            x_from_center = point_x - center_x
+            y_from_center = point_y - center_y
+
+            # Aplica a rotação manualmente
+            x_rotated = x_from_center * math.cos(math.radians(r)) - y_from_center * math.sin(math.radians(r))
+            y_rotated = x_from_center * math.sin(math.radians(r)) + y_from_center * math.cos(math.radians(r))
+
+            # Adiciona as coordenadas do centro de rotação de volta
+            x_rotated += center_x
+            y_rotated += center_y
+
+            glVertex2f(x_rotated, y_rotated)
             glColor3f(point['color'][0], point['color'][1], point['color'][2])
         glEnd()
 
